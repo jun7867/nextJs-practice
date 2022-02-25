07f0cdb9dcc0fe1db66617d7273d676c62429e62
남준영 <jjune095@naver.com>
2022-02-25 16:29:10 +0900

    feat: navbar router 추가

---
+++ components/Navbar.js
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+export default function Navbar() {
+  const router = useRouter();
+  console.log(router);
+
+  return (
+    <nav>
+      <Link href="/">
+        <a style={{ color: router.pathname === '/' ? 'red' : 'black' }}>Home</a>
+      </Link>
+      <Link href="/about">
+        <a style={{ color: router.pathname === '/about' ? 'red' : 'black' }}>
+          About
+        </a>
+      </Link>
+    </nav>
+  );
+}
